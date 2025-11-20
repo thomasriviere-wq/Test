204e17d5e9ce3e0c4b6ee5bfd34837831dd763a1
thomasriviere-wq <thomas.riviere@oteria.fr>
2025-11-20 16:58:06 +0000

    njun

--- bonjour.js
+++ bonjour.js
@@ -1,10 +1,5 @@
 <!doctype html>
 <html lang="fr">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Hello</title>
-  </head>
   <body>
     <script>
       alert("hello");
